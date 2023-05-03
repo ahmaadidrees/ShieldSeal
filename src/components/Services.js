a6efcac549e143cdcd5ff041c226856974553d5e
Ahmaad Idrees <maad@Ahmaads-MBP-2.lan>
2023-05-03 14:58:11 -0700

    fixed more bugs, added a couple transitions

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,35 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Container, Button } from 'react-bootstrap';
 import './Services.css';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+
 
 function Services() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      const windowHeight = window.innerHeight;
-      const servicesTop = document.querySelector('.services').offsetTop;
-
-      if (scrollTop + windowHeight > servicesTop) {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+ 
 
   return (
-    <div className={`services ${isVisible ? 'services--visible' : ''}`}>
+    <div className='services'>
       <Container className="services-container">
         <h1 className="services-title">Our Services</h1>
 
         <div className="row">
+        <CSSTransition
+          in={true}
+          appear={true}
+          timeout={1000}
+          classNames="service">
+        
           <div className="col-md-4">
             <h3 className="service-name">Sealcoating</h3>
-            <img src='/Images/sealcoat.jpeg' alt="Sealcoating" className="service-img" />
+            <Link to = {"/sealcoat"}>
+            <img onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} src='/Images/sealcoat.jpeg' alt="Sealcoating" className="service-img" />
+            </Link>
             <div className="service-description">
               <p>
               Asphalt sealcoating on a regular basis will more than triple the life of your parking lot.
@@ -37,10 +32,17 @@ function Services() {
               <Button as = {Link} to = '/sealcoat' onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} variant="warning" className="service-button">Learn More</Button>
             </div>
           </div>
-
+          </CSSTransition>
+          <CSSTransition
+          in={true}
+          appear={true}
+          timeout={1000}
+          classNames="service">
           <div className="col-md-4">
             <h3 className="service-name">Crack Filling</h3>
-            <img src='/Images/crackfilling.jpeg' alt="Crack Filling" className="service-img" />
+            <Link to = "/crack">
+            <img onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} src='/Images/crackfilling.jpeg' alt="Crack Filling" className="service-img" />
+            </Link>
             <div className="service-description">
               <p>
               Crack sealing is the first line of defence against water penetrating your asphalt.
@@ -48,10 +50,19 @@ function Services() {
               <Button as = {Link} to = '/crack' variant="warning" className="service-button" onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} >Learn More</Button>
             </div>
           </div>
+          </CSSTransition>
+          <CSSTransition
+          in={true}
+          appear={true}
+          timeout={1000}
+          classNames="service">
 
+          
           <div className="col-md-4">
             <h3 className="service-name">Line Painting</h3>
-            <img src="/Images/LinePainting.jpeg" alt="Line Painting" className="service-img" />
+            <Link to = {"/line"}>
+            <img onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} src="/Images/LinePainting.jpeg" alt="Line Painting" className="service-img" />
+            </Link>
             <div className="service-description">
               <p>
               Line painting makes for a functional and safe parking lot for pedestrians and vehicles.
@@ -59,6 +70,7 @@ function Services() {
               <Button as = {Link} to= '/line'  variant="warning" className="service-button" onClick={()=>{window.scrollTo({ top: 0, behavior: 'smooth' });}} >Learn More</Button>
             </div>
           </div>
+          </CSSTransition>
         </div>
       </Container>
     </div>
